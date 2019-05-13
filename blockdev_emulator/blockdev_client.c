abab9072ebd877dd11ae80e1cd6f577df7025e34
peterhyun <peterhyun1234@gmail.com>
2019-05-13 23:14:12 +0900

    add debugged code about declaration

--- blockdev_emulator/blockdev_client.c
+++ blockdev_emulator/blockdev_client.c
@@ -19,7 +19,7 @@ void * send_msg(void * arg);
 void * recv_msg(void * arg);
 void error_handling(char * msg);
 	
-char msg[BUF_SIZE];
+int msg[BLOCK_SIZE];
 	
 int main(int argc, char *argv[])
 {
@@ -28,11 +28,50 @@ int main(int argc, char *argv[])
 	pthread_t snd_thread, rcv_thread;
 	void * thread_return;
 
+	if(argc!=3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	 }
+	
+	sock=socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	  
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+		error_handling("connect() error");
+	
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+	pthread_join(snd_thread, &thread_return);
+	pthread_join(rcv_thread, &thread_return);
+	close(sock);  
+	return 0;
+}
+	
+	
+	
+	
+/**************************************************************************************************
+	name : send_msg
+	func : 서버에게 명령을 보낸다.
+	implementation logic : Accessing a block device to bring or transmit data by "Always on server"
+	last edit : 2019.04.04
+***************************************************************************************************/
+void * send_msg(void * arg)   // send thread main
+{
+	int sock=*((int*)arg);
+
 	//버퍼 내부 설정
 	int buffer[BLOCK_SIZE];
 	int total = 0;
+	int i = 0;
 	float signature = 0.0;
 	srand(time(NULL));
+
+
 
 	while(i < BLOCK_SIZE)
 	{
@@ -52,49 +91,13 @@ int main(int argc, char *argv[])
 	printf("size of buffer : %d\n", sizeof(buffer));
 	printf("signature code of buffer : %f\n",signature);
 
-
-	if(argc!=3) {
-		printf("Usage : %s <IP> <port>\n", argv[0]);
-		exit(1);
-	 }
-	
-	sock=socket(PF_INET, SOCK_STREAM, 0);
-	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
-	  
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
-		error_handling("connect() error");
-	
-	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock, 'R', 1543, buffer);
-	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
-	pthread_join(snd_thread, &thread_return);
-	pthread_join(rcv_thread, &thread_return);
-	close(sock);  
-	return 0;
-}
-	
-	
-	
-	
-/**************************************************************************************************
-	name : send_msg
-	func : 서버에게 명령을 보낸다.
-	implementation logic : Accessing a block device to bring or transmit data by "Always on server"
-	last edit : 2019.04.04
-***************************************************************************************************/
-void * send_msg(void * arg, int rw, unsigned int block_number, unsigned int *data)   // send thread main
-{
-	int sock=*((int*)arg);
-
+	// cmd 설정
 	struct command cmd {
-		.rw = rw,
-		.block_number =block_number,
-		.reclica[0] = 0,
-		.reclica[1] = 5,
-		.reclica[2] = 3,
+		rw = 'W';
+		block_number = 321;
+		reclica[0] = 0;
+		reclica[1] = 5;
+		reclica[2] = 3;
 	};
 	size_t offset = 0;
 
